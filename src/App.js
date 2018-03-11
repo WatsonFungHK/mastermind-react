f1ca984856f07ee243a4818b950fae002ef4789a
WatsonFungHK <watson.cwfung@gmail.com>
2018-03-11 15:25:42 +0800

    properly render component and implement layout

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import * as React from 'react';
 import logo from './logo.svg';
-import './App.css';
 import Activity from './activity/Activity';
 import Timer from './timer/Timer';
 import History from './history/History'
+// import * as styles from './styles.css';
+import './styles.css';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <Activity />
-        <div>
-          <Timer />
-          <History />
+        <div className="topBar">
+          <Activity />
+        </div>
+        <div className="container">
+          <Timer class="left" />
+          <History className="right" />
         </div>
       </div>
     );
