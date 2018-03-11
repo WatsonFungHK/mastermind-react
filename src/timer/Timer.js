7a889772ac678bb7ae12a8a2c433fb8cfa0b4b22
WatsonFungHK <watson.cwfung@gmail.com>
2018-03-11 15:33:38 +0800

    constructor a working timer

--- src/timer/Timer.js
+++ src/timer/Timer.js
@@ -1,10 +1,30 @@
 import * as React from 'react';
 
 class Timer extends React.Component {
+	timeStamp;
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			time: new Date() ,
+		}
+	}
+
+	componentDidMount() {
+		this.timeStamp = setInterval(() => this.tick(), 1000)
+	}
+
+	componentWillMount() {
+		clearInterval(this.timeStamp)
+	}
+
+	tick() {
+		this.setState({time: new Date()})
+	}
 
 	render() {
 		return (
-			<h1>Timer</h1>
+			<h1>{this.state.time.toLocaleTimeString()}</h1>
 		)
 	}
 	
